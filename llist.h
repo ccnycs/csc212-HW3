4c0cc0209707aea5f2f58326af47f4ff527e4e88
Maliha Tabassum <45563960+Maliha006@users.noreply.github.com>
2019-04-07 09:16:02 -0400

    Update llist.h

--- llist.h
+++ llist.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_SAVITCH_LIST_H  
+#ifndef MAIN_SAVITCH_LIST_H
 #define MAIN_SAVITCH_LIST_H
 
 #include <cstdlib> // Provides size_t and NULL
@@ -8,44 +8,69 @@
 #include <iostream>
 #include "node.h"
 
-namespace nodespace{
-    class LList{
-	public:
-        LList(){
-            head_ptr=NULL;
-            tail_ptr=NULL;
-            list_length=0;
-        }
-        // functions modified from the linked list toolkit
-        std::size_t length(const node* head_ptr){return list_length;}
-        
-        //add to end, insert at position P
-        void append(const node::value_type& entry);
-        void insert(std::size_t position, const node::value_type& entry);   
-        
-        //find first occurrence
-        const node* search(const node::value_type& target);
-        const node* locate(std::size_t position);
-        
-        //remove by value, remove all
-        bool remove(const node::value_type& target);
-        void clear();
-        
-        //returns new list
-        LList* copy();
+namespace nodespace
+{
+class LList
+{
+public:
 
-        //additional functions to index into list and overwrite node
-        node* operator[] (size_t ind);
-        node& operator = (node::value_type i);
+    LList()
+    {
+        head_ptr=NULL;
+        tail_ptr=NULL;
+        list_length=0;
+    }
 
-    private:
-        node* head_ptr;
-        node* tail_ptr;
-        size_t list_length;
-    };
+    LList(node* head, node* tail, node::value_type siz)
+    {
+        head_ptr = head;
+        head_ptr->set_link(tail);
+        tail_ptr = tail;
+        list_length=siz;
+    }
+    // functions modified from the linked list toolkit
+    std::size_t length(const node* head_ptr)
+    {
+        return list_length;
+    }
+    node* get_head_ptr()
+    {
+        return head_ptr;
+    }
+    node* get_tail_ptr()
+    {
+        return tail_ptr;
+    }
+
+    //add to end, insert at position P
+    void append(const node::value_type& entry);
+    void insert(std::size_t position, const node::value_type& entry);
+
+    //find first occurrence
+    const node* search(const node::value_type& target);
+    const node* locate(std::size_t position);
+
+    //remove by value, remove all
+    bool remove(const node::value_type& target);
+    void clear();
+
+    //returns new list
+    LList* copy();
+
+    //additional functions to index into list and overwrite node
+    node* operator[] (size_t ind);
+    node& operator = (node::value_type i);
+
+private:
+    node* head_ptr;
+    node* tail_ptr;
+    size_t list_length;
+};
 
 std::list<std::tuple<node::value_type, int>> frequency(LList data);
 }
 #endif
+
+
 
 		
